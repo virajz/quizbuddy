ba6a5e2c4b288c19dcb711df10b4ba4e60baf6c0
Viraj Zaveri <44199181+virajz@users.noreply.github.com>
2025-09-05 15:38:32 +0530

    Install Deps

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyARwHBGIUV5dmT_3u59E6QxlhW79-gg_MA",
+    authDomain: "quizbuddy-5c672.firebaseapp.com",
+    projectId: "quizbuddy-5c672",
+    storageBucket: "quizbuddy-5c672.firebasestorage.app",
+    messagingSenderId: "639106569114",
+    appId: "1:639106569114:web:e5293df53e707f5e75a023"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
